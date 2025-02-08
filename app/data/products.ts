f155952c9c68bea671e2e1807c17d27708b5b7f5
B khushal <khushalprasad242@gmail.com>
2025-02-08 20:13:34 +0530

    Update products.ts

--- app/data/products.ts
+++ app/data/products.ts
@@ -12,28 +12,28 @@ export const products: Product[] = [
     name: "Rose Bouquet",
     description: "A beautiful bouquet of fresh roses",
     price: 29.99,
-    image: "/images/img2.jpeg",
+    image: "/app/images/img2.jpeg",
   },
   {
     id: 2,
     name: "Tulip Arrangement",
     description: "Colorful tulips arranged in a vase",
     price: 34.99,
-    image: "/images/img3.jpeg",
+    image: "/app/images/img3.jpeg",
   },
   {
     id: 3,
     name: "Sunflower Bunch",
     description: "Bright and cheerful sunflowers",
     price: 24.99,
-    image: "/images/img4.jpeg",
+    image: "/app/images/img4.jpeg",
   },
   {
     id: 4,
     name: "Orchid Plant",
     description: "Elegant orchid plant in a decorative pot",
     price: 39.99,
-    image: "/images/img5.jpeg",
+    image: "/app/images/img5.jpeg",
   },
 ]
 
